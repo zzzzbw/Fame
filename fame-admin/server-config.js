038e7e4e6ec6e0a1340a9e639fe7f7ee847fb93b
d9577 <920049380@qq.com>
2018-09-26 11:50:27 +0800

    fix(fame-admin): 修复server-config在vue-cli 3.0

--- fame-admin/server-config.js
+++ fame-admin/server-config.js
@@ -1,6 +1,9 @@
 const isProd = process.env.NODE_ENV === 'production'
 const localhost = 'http://127.0.0.1:9090/'
 // build的时候传入参数或者使用localhost
-const baseUrl = process.env.BASE_URL || localhost
-exports.isProd = isProd
-exports.api = isProd ? baseUrl : localhost
+const baseUrl = process.env.VUE_APP_API_URL || localhost
+const api = isProd ? baseUrl : localhost
+export default {
+  isProd,
+  api
+}
