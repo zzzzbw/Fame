4233846c7af0d03fe73fa55c4534d38c21a9cc5e
Your Name <you@example.com>
2019-05-16 22:58:32 +0800

    fix:自动创建目录

--- fame-docker/fame-util/backup_mysql.sh
+++ fame-docker/fame-util/backup_mysql.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 date=`date '+%Y-%m-%dT%H%M%S'`
 container=`docker ps | grep mysql | awk '{print $1}'`
+mkdir -p /opt/bak
 docker exec $container /usr/bin/mysqldump -u root --password=root fame > /opt/bak/${date}backup.sql
 ret=$?
 if [ $ret -ne 0 ];then
