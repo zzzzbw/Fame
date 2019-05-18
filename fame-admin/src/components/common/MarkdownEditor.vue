ddf4f10addaf2a4045c9b89bc85784558a75272f
zzzzbw <zzzzbw@gmail.com>
2019-05-19 01:13:32 +0800

    fix(fame-admin): 1. 修复新markdown编辑器全屏异常问题; 2. 新markdown编辑器组件化

---
+++ fame-admin/src/components/common/MarkdownEditor.vue
@@ -0,0 +1,92 @@
+<template>
+  <mavon-editor
+    v-model="content"
+    :toolbars="markdownOption.toolbars"
+    :codeStyle="markdownOption.codeStyle"
+    @fullScreen="markdownOption.fullScreen"
+  >
+  </mavon-editor>
+</template>
+
+<script>
+import { mavonEditor } from "mavon-editor";
+import "mavon-editor/dist/css/index.css";
+
+export default {
+  name: "MarkdownEditor",
+  components: {
+    mavonEditor
+  },
+  model: {
+    prop: "value",
+    event: "change"
+  },
+  props: {
+    value: {
+      type: String,
+      default: ""
+    }
+  },
+  data: function() {
+    return {
+      content: "",
+      markdownOption: {
+        codeStyle: "tomorrow",
+        toolbars: {
+          bold: true, // 粗体
+          italic: true, // 斜体
+          header: true, // 标题
+          underline: true, // 下划线
+          strikethrough: true, // 中划线
+          mark: true, // 标记
+          superscript: true, // 上角标
+          subscript: true, // 下角标
+          quote: true, // 引用
+          ol: true, // 有序列表
+          ul: true, // 无序列表
+          link: true, // 链接
+          imagelink: true, // 图片链接
+          code: true, // code
+          table: true, // 表格
+          fullscreen: true, // 全屏编辑
+          readmodel: true, // 沉浸式阅读
+          htmlcode: true, // 展示html源码
+          help: true, // 帮助
+          /* 1.3.5 */
+          undo: true, // 上一步
+          redo: true, // 下一步
+          trash: true, // 清空
+          /* 2.2.1 */
+          subfield: true, // 单双栏模式
+          preview: true // 预览
+        },
+        fullScreen: this.markdownFullScreen
+      }
+    };
+  },
+  methods: {
+    markdownFullScreen(status, value) {
+      if (status) {
+        this.$root.$emit("indexUp");
+      } else {
+        this.$root.$emit("indexDown");
+      }
+    }
+  },
+  watch: {
+    value(val) {
+      this.value = val;
+      this.content = this.value;
+    },
+    content() {
+      this.$emit("change", this.content);
+    }
+  }
+};
+</script>
+
+<style scoped>
+.markdown-body {
+  height: calc(90vh - 100px);
+}
+</style>
--- fame-admin/src/components/page/Page.vue
+++ fame-admin/src/components/page/Page.vue
@@ -10,11 +10,7 @@
             ></el-input>
           </el-form-item>
           <el-form-item prop="content">
-            <mavon-editor
-              :toolbars="markdownOption.toolbars"
-              :codeStyle="markdownOption.codeStyle"
-              v-model="page.content"
-            />
+            <markdown-editor v-model="page.content" />
           </el-form-item>
         </el-col>
         <el-col :xs="24" :sm="8" :md="8" :lg="8">
@@ -45,109 +41,73 @@
   </div>
 </template>
 
-<script type="text/ecmascript-6">
-import { mavonEditor } from "mavon-editor";
-import "mavon-editor/dist/css/index.css";
+<script>
+import MarkdownEditor from "../common/MarkdownEditor";
 
 export default {
   components: {
-    mavonEditor
+    MarkdownEditor
   },
-  data: function () {
+  data: function() {
     return {
-      markdownOption: {
-        codeStyle: "tomorrow",
-        toolbars: {
-          bold: true, // 粗体
-          italic: true, // 斜体
-          header: true, // 标题
-          underline: true, // 下划线
-          strikethrough: true, // 中划线
-          mark: true, // 标记
-          superscript: true, // 上角标
-          subscript: true, // 下角标
-          quote: true, // 引用
-          ol: true, // 有序列表
-          ul: true, // 无序列表
-          link: true, // 链接
-          imagelink: true, // 图片链接
-          code: true, // code
-          table: true, // 表格
-          fullscreen: true, // 全屏编辑
-          readmodel: true, // 沉浸式阅读
-          htmlcode: true, // 展示html源码
-          help: true, // 帮助
-          /* 1.3.5 */
-          undo: true, // 上一步
-          redo: true, // 下一步
-          trash: true, // 清空
-          /* 2.2.1 */
-          subfield: true, // 单双栏模式
-          preview: true // 预览
-        }
-      },
       page: {
-        id: '',
-        title: '',
-        content: '',
-        status: ''
+        id: "",
+        title: "",
+        content: "",
+        status: ""
       },
       rules: {
         title: [
-          { required: true, message: '文章标题必须输入', trigger: 'blur' }
+          { required: true, message: "文章标题必须输入", trigger: "blur" }
         ],
         content: [
-          { required: true, message: '文章内容不能为空', trigger: 'blur' }
+          { required: true, message: "文章内容不能为空", trigger: "blur" }
         ]
       }
-    }
+    };
   },
   methods: {
-    getPage () {
-      const id = this.$route.params.id
+    getPage() {
+      const id = this.$route.params.id;
       if (id) {
         this.$api.auth.getPage(id).then(data => {
-          this.page.id = data.data.id
-          this.page.title = data.data.title
-          this.page.content = data.data.content
-          this.page.status = data.data.status
-        })
+          this.page.id = data.data.id;
+          this.page.title = data.data.title;
+          this.page.content = data.data.content;
+          this.page.status = data.data.status;
+        });
       } else {
-        this.page.id = ''
-        this.page.title = ''
-        this.page.content = ''
-        this.page.status = this.$util.STATIC.STATUS_PUBLISH
+        this.page.id = "";
+        this.page.title = "";
+        this.page.content = "";
+        this.page.status = this.$util.STATIC.STATUS_PUBLISH;
       }
     },
-    savePage (formName) {
-      this.$refs[formName].validate((valid) => {
+    savePage(formName) {
+      this.$refs[formName].validate(valid => {
         if (valid) {
           this.$api.auth.savePage(this.page).then(() => {
-            this.$router.push('/admin/page')
+            this.$router.push("/admin/page");
             this.$message({
-              message: '发布自定义页面成功!',
-              type: 'success'
-            })
-          })
+              message: "发布自定义页面成功!",
+              type: "success"
+            });
+          });
         }
-      })
+      });
     },
-    onPublish () {
-      this.savePage('pageForm')
+    onPublish() {
+      this.savePage("pageForm");
     }
   },
-  mounted () {
-    this.getPage()
+  mounted() {
+    this.getPage();
   }
-}
+};
 </script>
 
 <style scoped>
 .el-select {
   width: 100%;
-}
-
-.markdown-body {
-  height: calc(90vh - 100px);
 }
 </style>
