f51018d2d017530b76362e31a2aaeda63fab08aa
zzzzbw <zzzzbw@gmail.com>
2019-06-21 18:03:31 +0800

    fix(fame-front): 修复sitemap域名错误

--- fame-front/config/sitemap-config.js
+++ fame-front/config/sitemap-config.js
@@ -1,11 +1,33 @@
 import api from '../plugins/api'
+import tools from '../plugins/tools'
+
+// 静态页面,通过routes设置其url
+const STATIC_ROUTE = ['/', '/about', '/archive', 'category']
 
 const config = {
   routes() {
-    return api
-      .getArticles(1, 999)
-      .then(res => res.data.list.map(article => '/article/' + article.id))
-  }
+    return api.getOptions().then(optionsRes => {
+      let url = ''
+      if (
+        optionsRes.success &&
+        optionsRes.data &&
+        optionsRes.data.blog_website
+      ) {
+        url = tools.formatWebsite(optionsRes.data.blog_website)
+      }
+      return api.getArticles(1, 999).then(articleRes => {
+        const routes = articleRes.data.list.map(
+          article => url + '/article/' + article.id
+        )
+        STATIC_ROUTE.forEach(route => {
+          routes.push(url + route)
+        })
+        console.log(routes)
+        return routes
+      })
+    })
+  },
+  exclude: STATIC_ROUTE
 }
 
 export default {
