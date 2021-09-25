dff6b20f54a8cf979d6b2383d98315b831d23d4e
zzzzbw <zzzzbw@gmail.com>
2021-09-25 23:02:39 +0800

    feat(fame-admin): 文章导入功能

--- fame-admin/src/components/page/Article.vue
+++ fame-admin/src/components/page/Article.vue
@@ -134,6 +134,28 @@
                   </el-row>
                 </el-button-group>
               </el-form-item>
+              <el-form-item>
+                <el-button-group>
+                  <el-row>
+                    <el-upload
+                      ref="importUpload"
+                      class="inline-upload"
+                      accept="md"
+                      :show-file-list="false"
+                      :action="backup.importAction"
+                      :data="backup.importData"
+                      :with-credentials="true"
+                      :before-upload="beforeImport"
+                      :on-success="successImport"
+                      :on-error="errorImport"
+                    >
+                    </el-upload>
+                    <el-button size="small" @click="handleImport"
+                      >导入</el-button
+                    >
+                  </el-row>
+                </el-button-group>
+              </el-form-item>
             </div>
           </div>
         </el-col>
@@ -182,6 +204,7 @@
 </template>
 
 <script>
+import serverConfig from '../../../server-config'
 import MarkdownEditor from '../common/MarkdownEditor'
 import MediaItem from '../common/MediaItem'
 import Upload from '../common/Upload'
@@ -222,6 +245,12 @@ export default {
       tags: [],
       categories: [],
       flagFalse: false,
+      backup: {
+        importAction: serverConfig.api + 'api/admin/backup/import',
+        importData: {
+          articleId: null,
+        },
+      },
       mediaDialogData: {
         mediaData: [],
         total: 0,
@@ -295,6 +324,41 @@ export default {
           this.$util.message.error('获取分类列表失败')
         }
       })
+    },
+    handleImport() {
+      let self = this
+      this.$confirm('原有文章内容将被替换, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'danger',
+      })
+        .then(() => {
+          self.$refs['importUpload'].$refs['upload-inner'].handleClick()
+        })
+        .catch(() => {})
+    },
+    beforeImport(file) {
+      let fileName = file.name
+
+      const size = file.size / (1024 * 1024)
+      if (size > 10) {
+        this.$util.message.error(fileName + '大于10m')
+        return false
+      }
+
+      this.backup.importData.articleId = this.article.id
+    },
+    successImport(response, file) {
+      if (response.success) {
+        this.$util.message.success('上传' + file.name + '成功!')
+      } else {
+        this.$util.message.error('上传' + file.name + '失败!' + response.msg)
+      }
+      this.getArticle()
+    },
+    errorImport(err, file) {
+      this.$util.message.error('上传' + file.name + '失败!')
+      console.log(err)
     },
     showMediaDialog(page = 1) {
       this.isMobile = document.body.clientWidth < 768
@@ -395,6 +459,10 @@ export default {
 
 .el-date-editor {
   width: 100%;
+}
+
+.inline-upload {
+  display: inline;
 }
 
 .admin-page {
